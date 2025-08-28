31792ccd5ab0cd79132dbd5817018e6df55d83a8
Yoshily9 <113186151+yoshily9x@users.noreply.github.com>
2025-08-28 22:50:42 +0800

    submit styles adjust

--- components/NotionPage.tsx
+++ components/NotionPage.tsx
@@ -20,7 +20,7 @@ import { searchNotion } from '@/lib/search-notion'
 import { useDarkMode } from '@/lib/use-dark-mode'
 
 import { Footer } from './Footer'
-import { GitHubShareButton } from './GitHubShareButton'
+//import { GitHubShareButton } from './GitHubShareButton'
 import { Loading } from './Loading'
 import { NotionPageHeader } from './NotionPageHeader'
 import { Page404 } from './Page404'
@@ -277,11 +277,10 @@ export const NotionPage: React.FC<types.PageProps> = ({
         mapPageUrl={siteMapPageUrl}
         mapImageUrl={mapImageUrl}
         searchNotion={config.isSearchEnabled ? searchNotion : null}
-        pageAside={pageAside}
+        //pageAside={pageAside}
         footer={footer}
       />
 
-      <GitHubShareButton />
     </>
   )
 }
